956c2832ca291cbdb3d923890bb9c16679c3cd6e
DT_sameer <sameer.shaik@dispatchtrack.com>
2024-07-19 16:02:48 +0530

    Player Field Updated

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,13 @@
 class Player < ApplicationRecord
   belongs_to :team
+  
+  enum role: { allrounder: 'allrounder', bowler: 'bowler', batsman: 'batsman' }
+  
+  validates :name, presence: true
+  validates :age, presence: true, numericality: { only_integer: true }
+  validates :position, presence: true
+  validates :team_id, presence: true
+  validates :role, presence: true
+  validates :is_captain, inclusion: { in: [true, false] }
+  validates :is_active, inclusion: { in: [true, false] }
 end
