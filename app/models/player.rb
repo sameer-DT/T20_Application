9febc05c0fa465cbde4ee9755867858133d833bd
DT_sameer <sameer.shaik@dispatchtrack.com>
2024-07-19 18:36:47 +0530

    Validations

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,13 +1,16 @@
 class Player < ApplicationRecord
   belongs_to :team
-  
-  enum role: { allrounder: 'allrounder', bowler: 'bowler', batsman: 'batsman' }
-  
+
+  ROLES = ['Forward', 'Midfielder', 'Defender', 'Goalkeeper'].freeze
+
   validates :name, presence: true
   validates :age, presence: true, numericality: { only_integer: true }
   validates :position, presence: true
-  validates :team_id, presence: true
-  validates :role, presence: true
+  validates :team, presence: true
+  validates :role, inclusion: { in: ROLES }
+  validates :description, length: { maximum: 500 }
+
+
   validates :is_captain, inclusion: { in: [true, false] }
   validates :is_active, inclusion: { in: [true, false] }
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,9 @@
 class Team < ApplicationRecord
   has_many :players
-
+  
+  validates :name, presence: true, uniqueness: true
+  validates :country, presence: true
+  validates :founded, presence: true, numericality: { only_integer: true, greater_than: 1901 }
+  validates :description, length: { maximum: 500 } # Adjust length as needed
 end
+
